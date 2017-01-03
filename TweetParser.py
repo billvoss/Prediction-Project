f52ff8f4a41e6a5c94923af00304dc99a459aa11
Adam <adam.kitto@hotmail.com>
2017-01-03 19:41:14 +1030

    Tweet Parser

--- TweetParser.py
+++ TweetParser.py
@@ -5,14 +5,13 @@ def TweetParser(Tweet , Team):
     new_string = []
     count = 0
     count2 = 0
-    print(teamDF)
     while count < 11:
         count2 = 0
         while count2 < 22:
             #print(teamDF.loc[count2,'Sydney Sixers'])
             try :
-                if my_string[count] in teamDF.loc[count2,Team]:
-                    new_string.append(teamDF.loc[count2 , Team])
+                if my_string[count] in teamDF.iloc[count2,Team]:
+                    new_string.append(teamDF.iloc[count2 , Team])
             except :
                 count2 = count2
             count2 += 1
