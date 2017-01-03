5d4fa5de1c9c58dbb55a85b2dfc28ac6e20d806b
Adam <adam.kitto@hotmail.com>
2017-01-03 19:11:00 +1030

    Teams Working

--- TeamList.py
+++ TeamList.py
@@ -1,5 +1,5 @@
 import pandas as pd
-teamDF = pd.read_csv('BBLTeams.csv')
+teamDF = pd.read_excel('BBLTeams.xlsx')
 my_string = 'Hughes, Roy, Maddinson, Henriques, Haddin, Billings, Botha, Abbott, Dwarshuis, Somerville, Thornton'
 my_string = my_string.split(', ')
 new_string = []
@@ -9,7 +9,7 @@ count2 = 0
 print(teamDF)
 while count < 11:
     count2 = 0
-    while teamDF.loc[count2,'Sydney Sixers'] is not 'NaN' and count2 < 20:
+    while count2 < 22:
         #print(teamDF.loc[count2,'Sydney Sixers'])
         try :
             if my_string[count] in teamDF.loc[count2,'Sydney Sixers']:
